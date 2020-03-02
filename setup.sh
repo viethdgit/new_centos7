958f6983e74544e9a3bc0c7ee638215350aa0077
viethdgit <uocthem1dieu@gmail.com>
2020-03-02 11:04:20 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+\cp conf/limits.conf /etc/security/limits.conf
+\cp conf/selinux /etc/sysconfig/selinux
+\cp conf/sysctl.conf /etc/sysctl.conf
+\cp conf/rc.local /etc/rc.d/rc.local
+\cp conf/os/cron.allow /etc/cron.allow
+
+yum install epel-release ntpdate iptables-services net-tools wget -y
+
+systemctl stop postfix
+systemctl disable postfix
+
+systemctl disable firewalld
+systemctl stop firewalld
+
+systemctl start iptables-services
+systemctl enable iptables-services
+
+echo 'never' > /sys/kernel/mm/transparent_hugepage/enabled
+echo 'never' > /sys/kernel/mm/transparent_hugepage/defrag
+
+timedatectl set-timezone Asia/Ho_Chi_Minh
+ntpdate time.google.com
+crontab -l | { cat; echo '*/15 * * * * /usr/sbin/ntpdate time.google.com' ; } | crontab -
+
+chmod +x /etc/rc.d/rc.local
+systemctl enable rc-local
+systemctl start rc-local
+
+echo 'Done!'
+
